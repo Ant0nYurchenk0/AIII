c674c6166d1911aa932c69e9624d9c86b95e77ca
Anton Yurchenko <an.yurchenko.an@gmail.com>
2022-05-10 10:47:59 +0300

    gitignore

---
+++ AIII/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AIII.Startup))]
+namespace AIII
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
