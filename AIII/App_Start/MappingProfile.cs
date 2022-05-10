f600a54c54f71d15288f47935d3e966916f7eddd
Oleksandr Kosii <oleksandr.kosii@valtech.com>
2022-05-10 20:44:37 +0300

    Create Movie table,create movie table web api controller

---
+++ AIII/App_Start/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AIII.Dtos;
+using AIII.Models;
+using AutoMapper;
+
+namespace AIII.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>();
+        }
+    }
+}
---
+++ AIII/Dtos/MovieDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AIII.Dtos
+{
+    public class MovieDto
+    {
+        public int MovieId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Poster { get; set; }
+
+        public DateTime Year { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Type { get; set; }
+
+        public string Country { get; set; }
+
+        public string Cast { get; set; }
+
+        public string Plot { get; set; }
+
+        public double Budget { get; set; }
+
+        public double BoxOffice { get; set; }
+
+        public string RatingIMDB { get; set; }
+
+        public double SiteUserRating { get; set; }
+
+        public int GoodEmodjiAmount { get; set; }
+
+        public int BadEmodjiAmount { get; set; }
+    }
+}
