dd7b0c5cebffcff396a6a6eededdf3a21ff9690a
Oleksandr Kosii <oleksandr.kosii@valtech.com>
2022-05-18 13:21:33 +0300

    Change property type in UserRating table

---
+++ AIII/Migrations/202205181014572_ChangePropertyTypeInUserRatingTable1.cs
@@ -0,0 +1,18 @@
+﻿namespace AIII.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangePropertyTypeInUserRatingTable1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.UserRatings", "UserId", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.UserRatings", "UserId", c => c.Int(nullable: false));
+        }
+    }
+}
