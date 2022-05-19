c5b9c49f57e89adc05a7cb03810aadb374b95e3a
Oleksandr Kosii <oleksandr.kosii@valtech.com>
2022-05-19 13:04:57 +0300

    Add like/dislike functionality

---
+++ AIII/Controllers/UserRatingController.cs
@@ -0,0 +1,53 @@
+﻿using AIII.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AIII.Controllers
+{
+    public class UserRatingController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public UserRatingController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult IncrementLike(string movieId)
+        {
+            var userRating = _context.UserMovieRating.FirstOrDefault(r => r.MovieId == movieId && r.UserId == User.Identity.Name);
+
+            if(userRating.LikesAmount == 0)
+                userRating.LikesAmount += 1;
+            else
+                userRating.LikesAmount -= 1;
+
+            _context.SaveChanges();
+
+            if (!movieId.StartsWith("aaa"))
+                return RedirectToAction("GetMovie", "Imdb", new { id = movieId });
+            else
+                return RedirectToAction("GetMovie", "CustomMovie", new { id = movieId });
+        }
+
+        public ActionResult IncrementDislike(string movieId)
+        {
+            var userRating = _context.UserMovieRating.FirstOrDefault(r => r.MovieId == movieId && r.UserId == User.Identity.Name);
+
+            if (userRating.DislikesAmount == 0)
+                userRating.DislikesAmount += 1;
+            else
+                userRating.DislikesAmount -= 1;
+
+            _context.SaveChanges();
+
+            if (!movieId.StartsWith("aaa"))
+                return RedirectToAction("GetMovie", "Imdb", new {id = movieId});
+            else
+                return RedirectToAction("GetMovie", "CustomMovie", new { id = movieId });
+        }
+    }
+}
