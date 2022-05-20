a0773ac4b867c639c97df6d41418ddc70a1d1f4e
Oleksandr Kosii <oleksandr.kosii@valtech.com>
2022-05-20 09:57:46 +0300

    Change CustomMovie model

---
+++ AIII/Migrations/202205200652471_AddVideoPropertyInCustomMovieModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AIII.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddVideoPropertyInCustomMovieModel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CustomMovies", "Video", c => c.String(nullable: false));
+            DropColumn("dbo.UserRatings", "Watched");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.UserRatings", "Watched", c => c.Boolean());
+            DropColumn("dbo.CustomMovies", "Video");
+        }
+    }
+}
