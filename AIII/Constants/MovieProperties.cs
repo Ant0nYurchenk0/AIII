6cc96df6274bf39dcca73d881ca753941b17f531
Anton Yurchenko <an.yurchenko.an@gmail.com>
2022-05-25 14:31:56 +0300

    bug fix

--- AIII/Constants/MovieProperties.cs
+++ AIII/Constants/MovieProperties.cs
@@ -9,16 +9,30 @@ namespace AIII.Constants
             { "Comedy", "comedy"},
             { "Action", "action"},
             { "Family", "family"},
+            { "History", "history"},
+            { "Mystery", "mystery"},
+            { "Sci-fi", "sci_fi"},
+            { "War", "war"},
+            { "Adventure", "adventure"},
+            { "Crime", "crime"},
+            { "Fantasy", "fantasy"},
+            { "Horror", "horror"},
+            { "News", "news"},
+            { "Sport", "sport"},
+            { "Western", "western"},
+            { "Animation", "animation"},
+            { "Documentary", "documentary"},
+            { "Film-Noir", "film_noir"},
+            { "Music", "music"},
+            { "Reality-TV", "reality_tv"},
+            { "Talk-Show", "talk_show"},
+            { "Biography", "biography"},
+            { "Drama", "drama"},
+            { "Game-Show", "game_show"},
+            { "Musical", "musical"},
+            { "Romance", "romance"},
+            { "Thriller", "thriller"},
         };
-        public static readonly Dictionary<string, string> Types = new Dictionary<string, string>
-        {
-            { "Feature Film", "feature"},
-            { "TV series", "tv_series"},
-        };
-        public static readonly Dictionary<string, string> Countries = new Dictionary<string, string>
-        {
-            { "USA", "us"},
-            { "United Kingdom", "gb"},
-        };
+
     }
 }
